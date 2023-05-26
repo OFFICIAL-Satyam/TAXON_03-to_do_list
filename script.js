53060b49af93b429d631d280ed2207514398affd
Satyam Sharma <98421460+OFFICIAL-Satyam@users.noreply.github.com>
2023-05-27 03:07:15 +0530

    all done

---
+++ script.js
@@ -0,0 +1,41 @@
+const inputBox = document.getElementById("input-box");
+const listconatiner = document.getElementById("list-container");
+function AddTask()
+{
+    if (inputBox.value === '') {
+        alert("You must write something!!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML= inputBox.value;
+        listconatiner.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    saveData();
+}
+
+listconatiner.addEventListener("click",function(e){
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN")
+    {
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData()
+{
+    localStorage.setItem("data", listconatiner.innerHTML);
+}
+
+function showTask()
+{
+    listconatiner.innerHTML = localStorage.getItem("data");
+}
+showTask();
